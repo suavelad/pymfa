a6f5b052fcea18894aba0a7f83745ae3132dbc53
Sunday <sunnexajayi@gmail.com>
2024-10-17 03:29:42 +0100

    updated the module structure

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setuptools.setup(
     version="0.0.1",
     description="This helps you setup MFA with 3rd Party Authenticator",
     python_requires=">=3.10",   
-    packages=["mfa_authenticator"],
-    install_requires=["python-decouple","pyotp","qrcode"],
+    packages=["mfa"],
+    install_requires=["setuptools","python-decouple","pyotp","qrcode"],
 
 )
