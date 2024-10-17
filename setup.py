fc7f1528958818b8500c8caa18dbfdb7d6321805
Sunday <sunnexajayi@gmail.com>
2024-10-17 03:09:36 +0100

    modified the package

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ import setuptools
 
 
 setuptools.setup(     
-     name="sub-module",     
-     version="0.0.1",
-     description="This helps you setup MFA with 3rd Party Authenticator",
-     python_requires=">=3.10",   
-     packages=["mfa-authenticator"],
-     install_requires=["python-decouple","pyotp","qrcode"],
+    name="sub-module",     
+    version="0.0.1",
+    description="This helps you setup MFA with 3rd Party Authenticator",
+    python_requires=">=3.10",   
+    packages=["mfa-authenticator"],
+    install_requires=["python-decouple","pyotp","qrcode"],
 
 )
