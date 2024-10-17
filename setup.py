c1a1693b94e9227bc8a4d528708d781a8c8bb83e
Sunday <sunnexajayi@gmail.com>
2024-10-17 02:43:34 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+
+setuptools.setup(     
+     name="sub-module",     
+     version="0.0.1",
+     description="This helps you setup MFA with 3rd Party Authenticator",
+     python_requires=">=3.10",   
+     packages=["pymfa"],
+     install_requires=["python-decouple","pyotp","qrcode"],
+
+)
